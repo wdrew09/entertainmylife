761ecbab62e6e4b60256980cf53764b9aca78d2b
Andrew Wilson <awi279@uky.edu>
2020-05-12 23:17:02 -0400

    updates to header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,23 +13,20 @@ import { slide as Menu } from 'react-burger-menu'
 const Header = props => {
 
     const [pageWidth, setPageWidth] = useState()
-    const [computerStyle, setComputerStyle] = useState(true)
+
+    useEffect(() => {
+        setPageWidth(window.innerWidth)
+    }, [])
 
     const changingWidth = (width) => {
-        console.log(width)
         if (pageWidth != width) {
             setPageWidth(width)
-            if (width < 1150 && computerStyle != false) {
-                setComputerStyle(false)
-            } else if (width > 1149 && computerStyle != true) {
-                setComputerStyle(true)
-            }
         }
     }
 
     var menuStyles = {
         bmBurgerButton: {
-            position: 'fixed',
+            position: 'absolute',
             width: '36px',
             height: '30px',
             left: '36px',
@@ -49,21 +46,23 @@ const Header = props => {
             background: '#bdc3c7'
         },
         bmMenuWrap: {
-            position: 'fixed',
+            position: 'absolute',
             height: '100%'
         },
         bmMenu: {
             background: '#373a47',
             padding: '2.5em 1.5em 0',
             fontSize: '1.15em',
-            marginTop: '-35px'
+            marginTop: '-35px',
         },
         bmMorphShape: {
             fill: '#373a47'
         },
         bmItemList: {
             color: '#b8b7ad',
-            padding: '0.8em'
+            padding: '0.8em',
+            display: 'flex',
+            flexDirection: 'column'
         },
         bmItem: {
             display: 'inline-block'
@@ -77,32 +76,47 @@ const Header = props => {
     return (
         <div>
             <ReactResizeDetector handleWidth handleHeight onResize={(width, height) => changingWidth(width)} />
-            <div className={styles.TopButtons}>
-                {/* <button className={styles.SearchIcon}>
+
+            {pageWidth < 1100 ?
+                <div>
+                    <NavLink exact activeClassName="active" to="/homepage/1"><img className={styles.LogoImage} src={"/Entertain_My_Life_01.png"} /></NavLink>
+                    <Menu styles={menuStyles} customBurgerIcon={<FontAwesomeIcon style={{ margin: '0px 0px 0px 10px' }} icon={faBars} color='black' size='1x' />}>
+                        <NavLink activeClassName="active" to="/tech/1"><button className={styles.MenuButton}>Tech</button></NavLink>
+                        <NavLink activeClassName="active" to="/under20/1"><button className={styles.MenuButton}>Under 20</button></NavLink>
+                        <NavLink activeClassName="active" to="/fun/1"><button className={styles.MenuButton}>Fun</button></NavLink>
+                        <NavLink activeClassName="active" to="/gear/1"><button className={styles.MenuButton}>Gear</button></NavLink>
+
+
+                        <NavLink activeClassName="active" to="/pets/1"><button className={styles.MenuButton}>Pets</button></NavLink>
+                        <NavLink activeClassName="active" to="/kids/1"><button className={styles.MenuButton}>Kids</button></NavLink>
+                        <NavLink activeClassName="active" to="/home/1"><button className={styles.MenuButton}>Home</button></NavLink>
+                        <NavLink activeClassName="active" to="/shopsmall/1"><button className={styles.MenuButton}>Shop Small</button></NavLink>
+                    </Menu>
+                </div>
+                :
+                <div>
+                    <div className={styles.TopButtons}>
+                        {/* <button className={styles.SearchIcon}>
                     <FontAwesomeIcon icon={faSearch} color='black' size='lg' />
                 </button> */}
 
-            </div>
+                    </div>
+                    <div className={styles.SectionButtons}>
 
-            <Menu styles={menuStyles} customBurgerIcon={<FontAwesomeIcon style={{ margin: '10px 0px 0px 10px' }} icon={faBars} color='black' size='1x' />}>
-                <a id="home" className="menu-item" href="/">Home</a>
-                <a id="about" className="menu-item" href="/about">About</a>
-                <a id="contact" className="menu-item" href="/contact">Contact</a>
-            </Menu>
+                        <NavLink activeClassName="active" to="/tech/1"><button className={styles.SectionButtonsButton}>Tech</button></NavLink>
+                        <NavLink activeClassName="active" to="/under20/1"><button className={styles.SectionButtonsButton}>Under 20</button></NavLink>
+                        <NavLink activeClassName="active" to="/fun/1"><button className={styles.SectionButtonsButton}>Fun</button></NavLink>
+                        <NavLink activeClassName="active" to="/gear/1"><button className={styles.SectionButtonsButton}>Gear</button></NavLink>
 
-            <div className={styles.SectionButtons}>
-                <NavLink activeClassName="active" to="/tech/1"><button className={styles.SectionButtonsButton}>Tech</button></NavLink>
-                <NavLink activeClassName="active" to="/under20/1"><button className={styles.SectionButtonsButton}>Under 20</button></NavLink>
-                <NavLink activeClassName="active" to="/fun/1"><button className={styles.SectionButtonsButton}>Fun</button></NavLink>
-                <NavLink activeClassName="active" to="/gear/1"><button className={styles.SectionButtonsButton}>Gear</button></NavLink>
+                        <NavLink exact activeClassName="active" to="/homepage/1"><img className={styles.LogoImage} src={"/Entertain_My_Life_01.png"} /></NavLink>
 
-                <NavLink exact activeClassName="active" to="/homepage/1"><img className={styles.LogoImage} src={"/Entertain_My_Life_01.png"} /></NavLink>
-
-                <NavLink activeClassName="active" to="/pets/1"><button className={styles.SectionButtonsButton}>Pets</button></NavLink>
-                <NavLink activeClassName="active" to="/kids/1"><button className={styles.SectionButtonsButton}>Kids</button></NavLink>
-                <NavLink activeClassName="active" to="/home/1"><button className={styles.SectionButtonsButton}>Home</button></NavLink>
-                <NavLink activeClassName="active" to="/shopsmall/1"><button className={styles.SectionButtonsButton}>Shop Small</button></NavLink>
-            </div>
+                        <NavLink activeClassName="active" to="/pets/1"><button className={styles.SectionButtonsButton}>Pets</button></NavLink>
+                        <NavLink activeClassName="active" to="/kids/1"><button className={styles.SectionButtonsButton}>Kids</button></NavLink>
+                        <NavLink activeClassName="active" to="/home/1"><button className={styles.SectionButtonsButton}>Home</button></NavLink>
+                        <NavLink activeClassName="active" to="/shopsmall/1"><button className={styles.SectionButtonsButton}>Shop Small</button></NavLink>
+                    </div>
+                </div>
+            }
 
         </div>
     )
