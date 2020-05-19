aec5ee635a7d827632a0329c26f92bffe319a238
Andrew Wilson <awi279@uky.edu>
2020-05-18 21:38:50 -0400

    ssl secure?

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import HttpsRedirect from 'react-https-redirect';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HttpsRedirect>
+      <App />
+    </HttpsRedirect>
   </React.StrictMode>,
   document.getElementById('root')
 );
