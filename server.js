6db8040e0d665a1b38a6b40836c210664e456fea
Andrew Wilson <awi279@uky.edu>
2020-05-19 10:19:45 -0400

    redirect attempt

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+res.redirect(301, 'https://www.entertainmylife.com/' + req.originalUrl);
 
-
-app.use(function forceLiveDomain(req, res, next) {
-  // Don't allow user to hit Heroku now that we have a domain
-  var host = req.get('Host');
-  console.log(host)
-  if (host === 'entertainmylife.com' || host === 'http://entertainmylife.com' || host === 'http://www.entertainmylife.com' || 'https://entertainmylife.com') {
-    return res.redirect(301, 'https://www.entertainmylife.com/' + req.originalUrl);
-  }
-  return next();
-});
+// app.use(function forceLiveDomain(req, res, next) {
+//   // Don't allow user to hit Heroku now that we have a domain
+//   var host = req.get('Host');
+//   console.log(host)
+//   if (host === 'entertainmylife.com' || host === 'http://entertainmylife.com' || host === 'http://www.entertainmylife.com' || 'https://entertainmylife.com') {
+//     return res.redirect(301, 'https://www.entertainmylife.com/' + req.originalUrl);
+//   }
+//   return next();
+// });
 
 
 app.get('/*', function (req, res) {
