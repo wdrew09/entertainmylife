7745425f0e58c987088bcc827109ab5c3efb2f97
Andrew Wilson <awi279@uky.edu>
2020-05-21 14:11:10 -0400

    https attempt #?

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
+var sslRedirect = require('heroku-ssl-redirect');
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
+app.use(sslRedirect());
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 // res.redirect(301, 'https://www.entertainmylife.com/' + req.originalUrl);
