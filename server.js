5f29927478c5487e67216958fbe1e4bf67b05556
Andrew Wilson <awi279@uky.edu>
2020-05-21 14:23:53 -0400

    sadgf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var sslRedirect = require('heroku-ssl-redirect');
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(sslRedirect());
+// app.use(sslRedirect());
 app.use(sslRedirect(['production'], 301));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
