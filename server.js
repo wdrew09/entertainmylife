20784a14ab068f021657ea006645c6986db78f69
Andrew Wilson <awi279@uky.edu>
2020-05-21 15:08:12 -0400

    add www attempt

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ var sslRedirect = require('heroku-ssl-redirect');
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-// app.use(sslRedirect());
+
+app.all(/.*/, function (req, res, next) {
+  var host = req.header("host");
+  if (host.match(/^www\..*/i)) {
+    next();
+  } else {
+    res.redirect(301, "http://www." + host);
+  }
+});
 app.use(sslRedirect(['production'], 301));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
